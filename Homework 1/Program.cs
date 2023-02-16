e5b9bf1041e495c8c168b7721a00b96537c77e71
Timur Kamilov <d3kazakhstan@gmail.com>
2023-02-16 12:33:01 +0600

    Done Homework 1

--- Homework 1/Program.cs
+++ Homework 1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7->max = 7
+// a = 2 b = 10->max = 10
+// a = -9 b = -3->max = -3
+
+Console.Write("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum > secondNum)
+{
+    Console.Write(firstNum);
+    Console.Write(" является большим числом.");
+}
+else
+{
+    Console.Write(secondNum);
+    Console.Write(" является большим числом.");
+}
