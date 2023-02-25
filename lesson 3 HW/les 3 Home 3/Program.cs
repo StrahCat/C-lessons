fdb83159ecad837840e7f9253aba74e7132a6072
Timur Kamilov <d3kazakhstan@gmail.com>
2023-02-25 14:24:36 +0600

    just add Lesson 3 Homework 3 finish it

---
+++ lesson 3 HW/les 3 Home 3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int promptInput(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+void prompt(string message)
+{
+    System.Console.WriteLine(message);
+}
+
+void numSq(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int result = i * i;
+        Console.Write($"{result} ");
+    }
+}
+
+int userNum = promptInput("Напишите число но не слишком длинное: ");
+prompt($"Список квдратов чисел от 1 до {userNum}:");
+numSq(userNum);
